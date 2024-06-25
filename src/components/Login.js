669a3d3d254bfff5e781f292575ce7379d883cdb
Navneet Singh <snavneet795@gmail.com>
2024-06-26 01:14:59 +0530

    header logo + cover img

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import Header from "./Header";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+      <Header />
+      <img
+        src="
+https://assets.nflxext.com/ffe/siteui/vlv3/a56dc29b-a0ec-4f6f-85fb-50df0680f80f/2f8ae902-8efe-49bb-9a91-51b6fcc8bf46/IN-en-20240617-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+        alt="cover-image"
+      />
+    </div>
+  );
+};
 
-export default Login
+export default Login;
